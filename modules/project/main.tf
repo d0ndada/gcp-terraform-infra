6a298e7a0e9d9ecbaf2f1b6b96c619ebd846a6b3
d0ndada <pintoavila09@gmail.com>
2025-07-24 21:05:40 +0200

    feat: update module project

---
+++ modules/project/main.tf
@@ -0,0 +1,31 @@
+resource "google_project" "project" {
+  name       = var.project_name
+  project_id = var.project_id
+  org_id     = var.org_id
+
+  billing_account = var.billing_account
+
+  deletion_policy = var.deletion_policy
+
+  labels = var.labels  
+}
+
+
+resource "time_sleep" "wait_60_seconds" {
+    create_duration = "60s"
+    depends_on = [google_project.project]
+}
+
+resource "google_project_service" "apihub_service" {
+    project = google_project.project.project_id
+    service = "apihub.googleapis.com"
+    depends_on = [ time_sleep.wait_60_seconds ]
+}
+
+resource "google_apihub_host_project_registration" "apihub_host_registration" {
+    project = google_project.project.project_id
+    location = var.location
+    host_project_registration_id = google_project.project.project_id
+    gcp_project = "projects/${google_project.project.project_id}"
+    depends_on = [google_project_service.apihub_service]
+}
