7b68cfae25d83e9939f1a862e3dd3f38be239e60
d0ndada <pintoavila09@gmail.com>
2025-07-28 20:49:47 +0200

    fix: TFLint

--- modules/bootstrap/variables.tf
+++ modules/bootstrap/variables.tf
@@ -3,11 +3,6 @@ variable "project_name" {
   type        = string
 }
 
-variable "project_id" {
-  description = "The ID of the Google Cloud project."
-  type        = string
-}
-
 variable "account_id" {
   description = "The ID of the service account."
   type        = string
