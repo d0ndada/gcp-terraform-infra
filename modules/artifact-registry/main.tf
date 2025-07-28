e2fd53d994994198d65c7bab7d34f977664494d8
d0ndada <pintoavila09@gmail.com>
2025-07-28 20:43:03 +0200

    feat: usage of project id

--- modules/artifact-registry/main.tf
+++ modules/artifact-registry/main.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "artifact-repository" {
-  name          = var.repository_name
+  project = var.project_id
   location      = var.location
   repository_id = var.repository_id
   format        = var.format
---
+++ modules/project/output.tf
@@ -0,0 +1,4 @@
+output "id" {
+  description = "The ID of the project."
+  value       = google_project.project.project_id
+}
